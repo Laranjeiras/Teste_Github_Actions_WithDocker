f342ee045e17ce2e1efd39d4e7972afd41423bfe
Alexandre Laranjeiras <laranja22@hotmail.com>
2020-07-22 22:26:58 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Teste_Github_Actions_WithDocker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
